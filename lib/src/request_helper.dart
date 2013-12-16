f0fc76e569d44792784cdc8a5cd6c5a7260edc8d
Anthony Lai <anthony@mixbook.com>
2013-12-16 05:04:45 -0800

    Add support for not always setting content-type

--- lib/src/request_helper.dart
+++ lib/src/request_helper.dart
@@ -11,31 +11,31 @@ typedef HttpRequest RequestFactory();
 RequestFactory httpRequestFactory = () => new HttpRequest();
 
 class RequestHelper {
-  
+
   final String url;
   final String method;
   final Format format;
-  
+
   RequestHelper(this.method, this.url, this.format);
-  
+
   RequestHelper.get(this.url, this.format) : method = 'get';
-  
+
   RequestHelper.post(this.url, this.format) : method = 'post';
-  
+
   RequestHelper.put(this.url, this.format) : method = 'put';
-  
+
   RequestHelper.delete(this.url, this.format) : method = 'delete';
-  
+
   Future<HttpRequest> send([Object data]) {
     var completer = new Completer();
-    
+
     var request = httpRequestFactory();
     request.open(method, url);
-    
-    if (method != 'get') {
+
+    if (method != 'get' && format.contentType != null) {
       request.setRequestHeader('Content-Type', format.contentType);
     }
-    
+
     request.onLoad.listen((event) {
       if ((request.status >= 200 && request.status < 300) || request.status == 0) {
         completer.complete(request);
@@ -44,16 +44,16 @@ class RequestHelper {
       }
     });
     request.onError.listen((event) => completer.completeError(request));
-    
+
     if (data != null) {
       request.send(format.serialize(data));
     } else {
       request.send();
     }
-    
+
     return completer.future;
   }
-  
+
 }
 
 Logger _logger = new Logger("restful.request_helper");
--- test/request_helper_tests.dart
+++ test/request_helper_tests.dart
@@ -7,6 +7,10 @@ import 'package:restful/src/request_helper.dart';
 import 'package:restful/src/formats.dart';
 import 'request_mock.dart';
 
+class FormatContentTypeLess extends JsonFormat {
+  get contentType => null;
+}
+
 void testRequests() {
   group("Request", () {
     HttpRequestMock httpRequest;
@@ -16,23 +20,49 @@ void testRequests() {
       httpRequestFactory = () => httpRequest;
     });
 
-    test("should set header's Content-Type on POST requests", () {
-      var request = new RequestHelper.post('url', JSON_FORMAT);
-      request.send().then(expectAsync1((json) {
-        httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', JSON_FORMAT.contentType)).verify(happenedOnce);
-      }));
+    group("with a contentType on format", () {
+      var format = JSON_FORMAT;
+      test("should set header's Content-Type on POST requests", () {
+        var request = new RequestHelper.post('url', format);
+        request.send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(happenedOnce);
+        }));
+      });
+
+      test("should set header's Content-Type on PUT requests", () {
+        new RequestHelper.put('url', format).send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(happenedOnce);
+        }));
+      });
+
+      test("should not set header's Content-Type on GET requests", () {
+        new RequestHelper.get('url', format).send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(neverHappened);
+        }));
+      });
     });
 
-    test("should set header's Content-Type on PUT requests", () {
-      new RequestHelper.put('url', JSON_FORMAT).send().then(expectAsync1((json) {
-        httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', JSON_FORMAT.contentType)).verify(happenedOnce);
-      }));
-    });
+    group("without a contentType on format", () {
+      var format = new FormatContentTypeLess();
 
-    test("should not set header's Content-Type on GET requests", () {
-      new RequestHelper.get('url', JSON_FORMAT).send().then(expectAsync1((json) {
-        httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', JSON_FORMAT.contentType)).verify(neverHappened);
-      }));
+      test("should not set header's Content-Type on POST requests", () {
+        var request = new RequestHelper.post('url', format);
+        request.send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(neverHappened);
+        }));
+      });
+
+      test("should not set header's Content-Type on PUT requests", () {
+        new RequestHelper.put('url', format).send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(neverHappened);
+        }));
+      });
+
+      test("should not set header's Content-Type on GET requests", () {
+        new RequestHelper.get('url', format).send().then(expectAsync1((json) {
+          httpRequest.getLogs(callsTo('setRequestHeader', 'Content-Type', format.contentType)).verify(neverHappened);
+        }));
+      });
     });
   });
 }
